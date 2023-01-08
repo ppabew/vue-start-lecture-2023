e1c07d157cf5bc35c8da1f99bd7ecd6dea37b683
91274750 <ppabew@icloud.com>
2023-01-08 22:47:03 +0900

    added: vue-from examples addition.

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ str }}
+    <app-header v-bind:propsdata="str"
+                v-on:renew="renewStr"
+    ></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "App.vue",
+  components: {
+    "app-header": AppHeader
+  },
+  data: function() {
+    return {
+      str: "Header"
+    };
+  },
+  methods: {
+    renewStr: function() {
+      this.str = "hi";
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>{{propsdata}}</h1>
+    <button v-on:click="sendEvnet">Send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  props: ['propsdata'],
+  methods: {
+    sendEvnet: function () {
+      this.$emit('renew')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
